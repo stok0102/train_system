e3b38e82ba2a56c7b4788d90024e4409d5bad1ed
Caleb Stokka and Meaghan Jones <caleb+meaghan@>
2016-07-13 13:37:50 -0700

    allows a user to see all of the trains that have been created

--- spec/train_city_integration_spec.rb
+++ spec/train_city_integration_spec.rb
@@ -5,10 +5,20 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('adding a new train', {:type => :feature}) do
-  it('allows a user to click a train to see the tasks and details for it') do
+  it('allows a user to add a new train') do
     visit('/')
     fill_in('train_name', :with => "Meaghans Train")
     click_button('Add Train')
     expect(page).to have_content('Success!')
+    click_button('Return Home')
+  end
+end
+
+describe('viewing all the trains', {:type => :feature}) do
+  it "allows a user to see all of the trains that have been created" do
+    train = Train.new({:name => 'Meaghans Train'})
+    train.save()
+    visit('/')
+    expect(page).to have_content(train.name)
   end
 end
