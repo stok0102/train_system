8ee51b5e184d93d575c1aa35b75e6ff3181b7706
Caleb Stokka and Meaghan Jones <caleb+meaghan@>
2016-07-13 16:14:11 -0700

    got rid of success page, not working

--- app.rb
+++ app.rb
@@ -17,26 +17,32 @@ post("/trains") do
   name = params.fetch("train_name")
   train = Train.new({:name => name, :id => nil})
   train.save()
-  erb(:success)
+  @trains = Train.all()
+  @cities = City.all()
+  erb(:index)
 end
 
-delete("/train") do
-  @train = Train.find(params.fetch("train_id").to_i())
+delete("/train/:id") do
+  @train = Train.find(params.fetch("id").to_i())
   @train.delete()
   @trains = Train.all()
-  erb(:success)
+  @cities = City.all()
+  erb(:index)
 end
 
 post("/cities") do
   name = params.fetch("city_name")
   city = City.new({:name => name, :id => nil})
   city.save()
-  erb(:success)
+  @trains = Train.all()
+  @cities = City.all()
+  erb(:index)
 end
 
-delete("/city") do
-  @city = City.find(params.fetch("city_id").to_i())
+delete("/city/:id") do
+  @city = City.find(params.fetch("id").to_i())
   @city.delete()
+  @trains = Train.all()
   @cities = City.all()
-  erb(:success)
+  erb(:index)
 end
