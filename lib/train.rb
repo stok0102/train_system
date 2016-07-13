4f1591bd6f03136a9dbcbdcc573552dc01695f6a
Caleb Stokka and Meaghan Jones <caleb+meaghan@>
2016-07-13 09:52:44 -0700

    add save and all method to train class

--- lib/train.rb
+++ lib/train.rb
@@ -5,4 +5,20 @@ class Train
     @name = attributes.fetch(:name)
     @id = attributes[:id]
   end
+
+  define_singleton_method(:all) do
+    returned_trains = DB.exec("SELECT * FROM trains;")
+    trains = []
+    returned_trains.each() do |train|
+      name = train.fetch("name")
+      id = train.fetch("id").to_i()
+      trains.push(Train.new({:name => name, :id => id}))
+    end
+    trains
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO trains (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -8,4 +8,17 @@ describe(Train) do
     end
   end
 
+  describe(".all") do
+    it "empty at first" do
+      expect(Train.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it "lets you save trains to the database" do
+      train = Train.new({:name => "Orient Express", :id => nil})
+      train.save()
+      expect(Train.all()).to(eq([train]))
+    end
+  end
 end
